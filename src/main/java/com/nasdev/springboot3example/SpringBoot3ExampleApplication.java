b60dd74663515a06ef677f4f4584ed1816b4f822
Nasser Boukehil <boukehil.nasreddine@gmail.com>
2023-04-29 02:11:57 +0200

    added cmd runner and implemented find by id

--- src/main/java/com/nasdev/springboot3example/SpringBoot3ExampleApplication.java
+++ src/main/java/com/nasdev/springboot3example/SpringBoot3ExampleApplication.java
@@ -1,7 +1,13 @@
 package com.nasdev.springboot3example;
 
+import com.nasdev.springboot3example.customer.Customer;
+import com.nasdev.springboot3example.customer.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class SpringBoot3ExampleApplication {
@@ -10,4 +16,14 @@ public class SpringBoot3ExampleApplication {
 		SpringApplication.run(SpringBoot3ExampleApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner runner(CustomerRepository customerRepository) {
+		return args -> {
+			Customer john = new Customer("John", "john@gmail.com", 23);
+			Customer jane = new Customer("Jane", "jane@gmail.com", 33);
+
+			List<Customer> customers = List.of(john, jane);
+			customerRepository.saveAll(customers);
+		};
+	}
 }
