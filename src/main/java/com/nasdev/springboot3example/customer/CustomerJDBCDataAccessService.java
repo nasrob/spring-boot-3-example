ba0f3eb7178af71da03e0b5835ef56357e20247b
Nasser Boukehil <boukehil.nasreddine@gmail.com>
2023-04-30 19:47:21 +0200

    implemented crud with jdbc

--- src/main/java/com/nasdev/springboot3example/customer/CustomerJDBCDataAccessService.java
+++ src/main/java/com/nasdev/springboot3example/customer/CustomerJDBCDataAccessService.java
@@ -29,7 +29,15 @@ public class CustomerJDBCDataAccessService implements CustomerDao {
 
     @Override
     public Optional<Customer> selectCustomerById(Integer id) {
-        return Optional.empty();
+        var sql = """
+                SELECT id, name, email, age
+                FROM customer
+                WHERE id = ?
+                """;
+
+        return jdbcTemplate.query(sql, customerRowMapper, id)
+                .stream()
+                .findFirst();
     }
 
     @Override
@@ -44,17 +52,37 @@ public class CustomerJDBCDataAccessService implements CustomerDao {
 
     @Override
     public boolean isExistsWithEmail(String email) {
-        return false;
+        var sql = """
+                SELECT id, name, email, age
+                FROM customer
+                WHERE email = ?
+                """;
+
+        return jdbcTemplate.query(sql, customerRowMapper, email)
+                .stream()
+                .anyMatch(customer -> customer.getEmail().equals(email));
     }
 
     @Override
     public void deleteCustomerById(Integer id) {
+        var sql = """
+                DELETE FROM customer
+                WHERE id = ?
+                """;
 
+        jdbcTemplate.update(sql, id);
     }
 
     @Override
     public boolean isExistsWithId(Integer customerId) {
-        return false;
+        var sql = """
+                SELECT COUNT(id)
+                FROM customer
+                WHERE id = ?
+                """;
+
+        Integer count = jdbcTemplate.queryForObject(sql, Integer.class, customerId);
+        return count != null && count > 0;
     }
 
     @Override
@@ -64,6 +92,19 @@ public class CustomerJDBCDataAccessService implements CustomerDao {
 
     @Override
     public void updateCustomer(Customer customer) {
+        if (customer.getName() != null) {
+            String sql = "UPDATE customer SET name = ? WHERE id = ?";
+            jdbcTemplate.update(sql, customer.getName(), customer.getId());
+        }
 
+        if (customer.getEmail() != null) {
+            String sql = "UPDATE customer SET email = ? WHERE id = ?";
+            jdbcTemplate.update(sql, customer.getEmail(), customer.getId());
+        }
+
+        if (customer.getAge() != null) {
+            String sql = "UPDATE customer SET age = ? WHERE id = ?";
+            jdbcTemplate.update(sql, customer.getAge(), customer.getId());
+        }
     }
 }
